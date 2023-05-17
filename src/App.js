9e2dc99240ef627c555ff39f055f461730452d6b
leyton lineburg <leyton@leytons-MacBook-Air.local>
2023-05-17 17:31:26 -0400

    random note selection

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
+let NOTES = {};
+let r = require.context('./notes', false, /\.png$/);
+  r.keys().forEach((item, img) => {
+    NOTES[item.replace('./', '')] = r(item);
+  });
+let notes = Object.keys(NOTES);
+
 function App() {
+
+  let selected = notes[Math.floor(notes.length * Math.random())]
+  console.log(selected)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>NoteLearner</h1>
+        <img src={NOTES[selected]} alt="note"/>
       </header>
     </div>
   );
